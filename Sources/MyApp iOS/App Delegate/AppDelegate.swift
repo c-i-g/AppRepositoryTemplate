367fe89c6fd82401553e3051f1449a4a0d85cc5c
PC Nguyen <code.in.grace@gmail.com>
2022-12-27 08:59:42 -0800

    remove firebase for the time being

--- Sources/MyApp iOS/App Delegate/AppDelegate.swift
+++ Sources/MyApp iOS/App Delegate/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import Firebase
+// import Firebase
 
 @UIApplicationMain
 final class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -20,7 +20,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     private func configureFrameworks() {
-        FirebaseApp.configure()
+        // FirebaseApp.configure()
     }
 
 }
